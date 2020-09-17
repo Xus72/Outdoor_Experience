6f47a36a6bd8848619fcf96b05cca3ab2fdb7eea
Xus72 <jexusth@hotmail.com>
2020-09-17 02:17:00 +0200

    creada pagina principal

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Proveedor, Actividad, Guia, Participante
+
+def index(request):
+    #Contador de actividades
+    num_actividades = Actividad.objects.all().count()
+    #Contador de guias
+    num_guias = Guia.objects.all().count()
+
+    return render(request, 'index.html', context={'num_actividades':num_actividades, 'num_guias':num_guias},)
